e7d694c262d8e0702906ea70b3cfbe05194ecf14
white1033 <k75715@gmail.com>
2012-01-07 18:21:14 +0800

    new algorithm

--- 021.pl
+++ 021.pl
@@ -4,23 +4,35 @@ use warnings;
 
 sub sum_of_divisors {
     my $num = shift;
-    my $sum = 1;
-    foreach my $x (2 .. sqrt $num) {
-        if ($num % $x == 0) {
-            $sum += ($x + $num / $x);
+    my ($prime, $sum, $tmp) = (2, 1);
+    while ($prime**2 <= $num && $num > 1) {
+        if (!($num % $prime)) {
+            $tmp = $prime**2;
+            $num /= $prime;
+            while (!($num % $prime)) {
+                $tmp *= $prime;
+                $num /= $prime;
+            }
+            $sum *= $tmp - 1;
+            $sum /= $prime - 1;
         }
+        if ($prime == 2) { $prime = 3 }
+        else { $prime += 2 }
     }
-    if ((sqrt $num) =~ /^[+-]?\d+$/) { #use regular expression to do is_integer 
-        $sum -= sqrt $num;
-    }
+    if ($num > 1) { $sum *= $num + 1 }
     return $sum
+}
+
+sub sum_of_proper_divisors {
+    my $num = shift;
+    return sum_of_divisors($num) - $num
 }
 
 my $sum;
 my $tmp;
 foreach my $num (2 .. 10_000) {
-    $tmp = sum_of_divisors($num);
-    if ($tmp != $num && sum_of_divisors($tmp) == $num) {
+    $tmp = sum_of_proper_divisors($num);
+    if ($tmp != $num && sum_of_proper_divisors($tmp) == $num) {
         $sum += $num;
     }
 }
