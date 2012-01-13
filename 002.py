e4963afd76122128dfc1bd223c201b35a5e7662c
white1033 <k75715@gmail.com>
2012-01-13 09:42:14 +0800

    python way :D

---
+++ 002.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+fib1, fib2 = 1, 1
+sum_fibs = 0
+while fib1 < 4000000:
+    if not fib1 % 2: sum_fibs += fib1
+    fib1, fib2 = fib2, fib1 + fib2
+
+print(sum_fibs)
