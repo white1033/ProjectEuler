30b63830d62db690fccd421d6cbaa6b02cde59b1
white1033 <k75715@gmail.com>
2012-01-29 02:12:47 +0800

    no message

--- 206.rb
+++ 206.rb
@@ -5,7 +5,7 @@ upper_bound_sqrt = (Math.sqrt(upper_bound) + 1).to_i
 
 while true
   break if (upper_bound_sqrt**2).to_s.match(/1.2.3.4.5.6.7.8.9/)
-  upper_bound_sqrt -=2
+  upper_bound_sqrt -= 2
 end
 
 puts upper_bound_sqrt*10
