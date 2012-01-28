6105888e30b1951c2c423dcff8544b006013b916
white1033 <k75715@gmail.com>
2012-01-29 01:21:33 +0800

    Simple brute force

---
+++ 206.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+upper_bound = 19_293_949_596_979_899
+upper_bound_sqrt = (Math.sqrt(upper_bound) + 1).to_i
+
+while true
+  break if (upper_bound_sqrt**2).to_s.match(/1.2.3.4.5.6.7.8.9/)
+  upper_bound_sqrt -=2
+end
+
+puts upper_bound_sqrt*10
